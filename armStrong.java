ca474de9877c75d98f4bdb8ba227f0f7391e9a54
Sudharsan1721-D <sudharsandhamodharan9361@gmail.com>
2024-09-03 12:23:09 +0530

    Update armStrong.java

--- armStrong.java
+++ armStrong.java
@@ -1,19 +1,31 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
 class HelloWorld {
     public static void main(String[] args) {
-        int n=515;
+        int n=153;
         int temp=n;
+        int num=temp;
+        int c=0;
         int rem=0;
-        int rev=0;
         while(temp>0){
-            rem=temp%10;
-            rev=rev*10+rem;
+            c++;
             temp=temp/10;
         }
-        if(rev==n){
-            System.out.print("ArmStrong");
+        int sum=0;
+        while(num>0){
+            rem=num%10;
+            sum=sum+(int)Math.pow((double)rem,(double)c);
+            num=num/10;
         }
-        else{
-            System.out.print("Not a ArmStrong");
-        }
+        System.out.println(sum);
+    if(sum==n){
+        System.out.print("It is Armstrong");
     }
+    else{
+        System.out.print("It is not a Armstrong");
+    }
+        
+    }
+    
 }
